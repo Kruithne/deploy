47d16dee0ca54ec26bf68f1f4367066695781725
Kruithne <kruithne@gmail.com>
2014-07-24 01:24:15 +0100

    Change: Show an error message when we're unable to delete a remote file.

--- deploy.php
+++ deploy.php
@@ -460,7 +460,8 @@
 			$remote_file = smoothSeparators($remote_location . $upload_file_name);
 
 			output('Deleting old file: ' . $remote_file);
-			ssh2_sftp_unlink($connection, $remote_file);
+			if (!ssh2_sftp_unlink($connection, $remote_file))
+				output('UNABLE to delete remote file: ' . $remote_file);
 		}
 	}
 
